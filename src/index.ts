b47a61ec32090eeb853d301c9a01d45deb098ca0
Raquel Moreira <raquel.moreira@nextpage.com.br>
2022-03-10 16:37:10 -0300

    api documentation

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import express from "express";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
+import swagger from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile));
 
 export { app };
